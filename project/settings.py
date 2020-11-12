246a23da754c89dc21aee5d3add1bf25f9bfce9c
akhil <mettildaakhil02@gmail.com>
2020-11-12 18:06:03 +0530

    added allowed hosts in settings.py

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_heroku
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '15qmf4^4=&oy=&qf(s7pw#7l5x%*=p_63n^iti)=8$xnaw*(--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','akhilalosiousmyprojectabc.herokuapp.com']
 
 
 # Application definition
